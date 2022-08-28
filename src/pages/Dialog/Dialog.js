e06325d812a60cb3ed574d36b21dcc3bbcdee697
semgoSE <semgoSE@gmail.com>
2022-08-28 19:56:13 +0300

    fix styles

--- src/pages/Dialog/Dialog.js
+++ src/pages/Dialog/Dialog.js
@@ -3,8 +3,7 @@ import styles from './Dialog.module.scss';
 export const Dialog = () => {
     return(
         <div className={styles.dialog}>
-            <span className={styles.header}>Дашборд диалога</span>
-
+            <span className={styles.header}>Dashboard</span>
         </div>
     );
 }
